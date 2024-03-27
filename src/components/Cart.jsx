1fcfa836075a22648f52cc669d0b06a3b928a59c
Elias Segura <eliasjsegura00@gmail.com>
2024-03-27 11:45:12 -0600

    mas cambios

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,33 @@
-import './Cart.css'
+import "./Cart.css";
 import { useId } from "react";
 import { CartIcon, ClearCartIcon } from "./Icons";
-import { useCart } from '../hooks/useCart';
+import { useCart } from "../hooks/useCart";
+
+//separamos el li del aside ul
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img
+        src={ thumbnail }
+        alt={ title }
+      />
+      <div>
+        <strong>{ title }</strong> - ${ price }
+      </div>
+
+      <footer>
+        <small> Qty: { quantity } </small>
+        <button onClick={ addToCart } >+</button>
+      </footer>
+    </li>
+  );
+}
 
 export function Cart() {
   const cartCheckboxId = useId();
-  //tnemos que traernos el carrito, y arreglar el carrito todo lo que tenemos
-  //clearCart es bastante sencillo que si le damos clic al botón, desaparecen los carritos seleccionados
-  const { cart, clearCart } = useCart();//pasamos onChange en button el clearCartIcon
+
+  //traemos la variable de estado que se llame addToCart
+  const { cart, clearCart, addToCart } = useCart();
 
   return (
     <>
@@ -16,28 +36,21 @@ export function Cart() {
       </label>
       <input id={cartCheckboxId} type="checkbox" hidden />
 
-      <aside className='cart'>
+      <aside className="cart">
         <ul>
-          <li>
-            <img 
-                src="https://cdn.dummyjson.com/product-images/1/thumbnail.jpg" 
-                alt="iPhone" 
-            />
-            <div>
-                <strong>iPhone</strong> - $1499
-            </div>
-
-            <footer>
-                <small>
-                    Qty: 1
-                </small>
-                <button>+</button>
-            </footer>
-          </li>
+          {
+            cart.map( product => (
+              <CartItem 
+                key={ product.id } 
+                addToCart={ () => addToCart(product) }  
+                { ...product } 
+              />
+            ))
+          }
         </ul>
 
-        <button style={{ background: '#E36414' }} onClick={ clearCart } >
-            <ClearCartIcon />
+        <button style={{ background: "#E36414" }} onClick={clearCart}>
+          <ClearCartIcon />
         </button>
       </aside>
     </>
--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,45 +1,71 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 //1. Crear el contexto
 export const CartContext = createContext();
 
-//2. Crear el proveedor
-export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+const initialState = []
 
-  const addToCart = (product) => {
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
+const reducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action
 
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart);
-      newCart[productInCartIndex].quantity += 1;
-      return setCart(newCart);
+  switch (actionType){
+    case 'ADD_TO_CART': {
+      const { id } = actionPayload
+      //verificar si el producto esta en el carrito
+      const productInCartIndex = state.findIndex( item => item.id === id)
+
+      //si esta en el carrito
+      if (productInCartIndex >= 0){
+        const newState = structuredClone(state)
+        
+        newState[productInCartIndex].quantity += 1
+        return newState
+      }
+
+      //si el producto no esta en el carrito
+      return [
+        ...state,
+        {
+          ...actionPayload,
+          quantity: 1
+        }
+      ]
     }
 
-    setCart((prevState) => [
-      ...prevState,
-      {
-        ...product,
-        quantity: 1,
-      },
-    ]);
-  };
+    case 'REMOVE_FROM_CART': {
+      const { id } = actionPayload
+      return state.filter(item => item.id != id)
+    }
 
-  //para remover del carrito
-  //con el setter y el previous state
-  const removeFromCart = product => {
-    setCart(prevState => prevState.filter(item => item.id != product.id));
-    //filtramos los productos que tengan un id diferente
-  }//luego lo pasamos en value del proveedor
+    case 'CLEAR_CART': {
+      return initialState
+    }
+  }
+}
 
-  const clearCart = () => {
-    setCart([]); //para limpiar el cart ponemos el array vacío
-  };
+
+//2. Crear el proveedor
+export function CartProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const addToCart = product => dispatch({
+    type: 'ADD_TO_CART',
+    payload: product
+  })
+
+  const removeFromCart = product => dispatch({
+    type: 'REMOVE_FROM_CART',
+    payload: product
+  })
+
+  const clearCart = () => dispatch({
+    type: 'CLEAR_CART'
+  })
 
   return (
     <CartContext.Provider
       value={{
-        cart,
+        cart:state,
         addToCart,
         removeFromCart,
         clearCart,
